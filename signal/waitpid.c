2b1377195c16217f4d7ae69e921835dcb4916ad4
DavidMusk <davidmusksun1993@gmail.com>
2020-06-11 10:13:44 +0800

    [waitpid] use WEXITSTATUS retrieves subprocess exit status
    Signed-off-by: DavidMusk <davidmusksun1993@gmail.com>

--- signal/waitpid.c
+++ signal/waitpid.c
@@ -2,10 +2,40 @@
 // Created by Steve on 4/21/2020.
 //
 
-#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#include "macro.h"
+
+static void foo() {
+#define VALUE 13 /* macro lifetime until #undef */
+    log("@%s %d", __func__, VALUE);
+#undef VALUE
+}
+
+static void bar() {
+#define VALUE "yes"
+    log("@%s %s", __func__, VALUE);
+#undef VALUE
+}
 
 int main() {
-    int status = 65280;
-    printf("%#x\n", status);
+    switch (fork()) {
+        case -1: {
+            log("fork: %m");
+            return 1;
+        }
+        case 0: {
+            log("subprocess: %ld", (long) getpid());
+//            pause();
+            exit(15);
+        }
+        default: {
+            int status = -1;
+            wait(&status);
+            log("child terminate status: %#x", status);
+        }
+    }
+    foo(), bar();
     return 0;
 }
