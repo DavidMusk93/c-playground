5042193ac2fa343f292e652ce18ede905657c5ed
DavidMusk <davidmusksun1993@gmail.com>
2020-07-09 15:32:15 +0800

    [ptrace] trace syscall
    @ref
      * https://man7.org/linux/man-pages/man2/ptrace.2.html
      * https://unix.stackexchange.com/questions/421750/where-do-you-find-the-syscall-table-for-linux
      * https://www.linuxjournal.com/article/6100
    
    Signed-off-by: DavidMusk <davidmusksun1993@gmail.com>

--- ptrace/trace-syscall.c
+++ ptrace/trace-syscall.c
@@ -1,5 +1,6 @@
 //
 // Created by Steve on 7/9/2020.
+// @ref https://www.linuxjournal.com/article/6100
 //
 
 #include <sys/ptrace.h>
@@ -7,3 +8,27 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <sys/user.h>
+#include <assert.h>
+
+#include "macro.h"
+
+#define USER_REGS_OFF(x) (&((struct user_regs_struct*)0)->x)
+
+MAIN() {
+    pid_t child;
+    long orig_eax;
+    child = fork();
+    if (child == 0) {
+        ptrace(PTRACE_TRACEME, 0, 0, 0);
+        execl("/bin/ls", "ls", NULL); /* NULL is different from 0 for compiler */
+    } else {
+        int status = -1;
+        waitpid(child, &status, 0);
+        assert(WIFSTOPPED(status) && WSTOPSIG(status) == SIGTRAP);
+        LOG("child status: %x", status);
+        orig_eax = ptrace(PTRACE_PEEKUSER, child, USER_REGS_OFF(orig_rax), 0); /* 59 for execve by `ausyscall --dump` */
+        LOG("the child made a syscall %ld", orig_eax);
+        ptrace(PTRACE_CONT, child, 0, 0);
+    }
+    return 0;
+}
