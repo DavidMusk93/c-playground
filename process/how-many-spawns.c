e0440ef980b113560b40128f4f1b95032bba9e80
Mingqiang Sun <davidmusksun1993@gmail.com>
2020-07-12 16:25:33 +0800

    add comments to zombie.c
    Signed-off-by: Mingqiang Sun <davidmusksun1993@gmail.com>

--- process/how-many-spawns.c
+++ process/how-many-spawns.c
@@ -1,5 +1,7 @@
 //
 // Created by Steve on 7/12/2020.
+// @ref http://www.spongeliu.com/123.html#comment-106762
+// BINARY TREE ANALYSIS WOULD BE BETTER
 //
 
 #include <unistd.h>
--- process/zombie.c
+++ process/zombie.c
@@ -8,6 +8,14 @@
 
 MAIN() {
     pid_t child;
+    /** ZOMBIE PROCESS (@ref https://man7.org/linux/man-pages/man5/proc.5.html)
+     * cat /proc/$child/status
+     *     State: Z (zombie)
+     *     Tgid:  23876 (thread group ID)
+     *     Ngid:  0 (NUMA group ID)
+     *     Pid:   24876 (thread ID)
+     *     PPid:  23875 (PID of parent process)
+     */
     switch ((child = fork())) {
         case -1: {
             LOG("fork: %m");
