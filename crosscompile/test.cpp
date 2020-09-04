dd200133c5324c3a91f4e2f65830685dbe9ff577
Mingqiang Sun <davidmusksun1993@gmail.com>
2020-09-04 17:24:07 +0800

    add cross compile demo
    Signed-off-by: Mingqiang Sun <davidmusksun1993@gmail.com>

---
+++ crosscompile/test.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Steve on 9/4/2020.
+//
+
+#include "test.h"
+
+#include <stdio.h>
+
+int add(int a,int b){
+    return a+b;
+}
+
+int main(){
+    int a=1,b=2;
+    printf("%d\n",add(a,b));
+}
---
+++ crosscompile/test.h
@@ -0,0 +1,10 @@
+//
+// Created by Steve on 9/4/2020.
+//
+
+#ifndef CROSS_COMPILE_TEST_H
+#define CROSS_COMPILE_TEST_H
+
+int add(int,int);
+
+#endif //CROSS_COMPILE_TEST_H
